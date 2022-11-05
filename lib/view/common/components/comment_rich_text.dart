d3ee41b765c35dd6cb6b1932307df3ff81abe569
morita <morita4492.0305@gmail.com>
2022-11-06 02:34:40 +0900

    add

--- lib/view/common/components/comment_rich_text.dart
+++ lib/view/common/components/comment_rich_text.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class CommentRichText extends StatefulWidget {
+  final String name;
+  final String text;
+
+  const CommentRichText({super.key, required this.name, required this.text});
+
+  @override
+  State<CommentRichText> createState() => _CommentRichTextState();
+}
+
+class _CommentRichTextState extends State<CommentRichText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
